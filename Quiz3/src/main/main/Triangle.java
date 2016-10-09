fda10bf9c8b00fc634d3a7ecc6d2ca654b4f6e3b
Jason <Jason@192.168.1.9>
2016-10-09 19:19:19 -0400

    -

---
+++ Quiz3/src/main/main/Triangle.java
@@ -0,0 +1,50 @@
+package main;
+
+public class Triangle {
+
+	private double side1;
+	private double side2;
+	private double side3;
+	
+	Triangle(){
+		side1 = 1.0;
+		side2 = 1.0;
+		side3 = 1.0;
+	}
+	
+	Triangle(double x, double y, double z) throws Exception{
+		if (x+y>z && x+z>y && y+z>x){
+			side1 = x;
+			side2 = y;
+			side3 = z;
+		}
+		else throw new Exception();
+	}
+	
+	public double getSide1(){
+		return side1;
+	}
+	
+	public double getSide2(){
+		return side2;
+	}
+	
+	public double getSide3(){
+		return side3;
+	}
+	
+	public double getPerimeter(){
+		return side1+side2+side3;
+	}
+	
+	public double getArea(){
+		double s = getPerimeter()/2;
+		double A = Math.sqrt(s * (s-side1) * (s-side2) * (s-side3));
+		return A;
+	}
+	
+	@Override
+	public String toString(){
+		return "Side 1 is " + side1 + ". Side 2 is " + side2 + ". Side 3 is " + side3 + ".";
+	}
+}
---
+++ Quiz3/src/test/main/TriangleTest.java
@@ -0,0 +1,37 @@
+package main;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TriangleTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void TestTriangle() throws Exception {
+		Triangle t = new Triangle(3.0,4.0,5.0);
+		assertEquals(12.0, t.getPerimeter());
+		assertEquals(6.0, t.getArea());
+		Triangle notReal = new Triangle(1.0,1.0,2.0);
+	}
+
+}
